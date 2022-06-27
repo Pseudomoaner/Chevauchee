21396f1ec7891454dcf9809e1b77b2df9bf954b3
omeacock <48593071+Pseudomoaner@users.noreply.github.com>
2022-06-28 01:25:09 +0200

    True surface coverage
    Adds data to estimate the true surface coverage of highly packed monolayers from high magnification subsurface images. Uses this data to correct for a slight under-estimate of the attacker-sensitive contact probabilities in the image analysis.

--- Image analysis/measureSensitiveKillerContactProb.m
+++ Image analysis/measureSensitiveKillerContactProb.m
@@ -6,10 +6,11 @@ function pK = measureSensitiveKillerContactProb(imgS,imgK,pxSize)
 % aByL = ((l-w)*w + pi*(w/2)^2)/(pi*w + (l-w)*2); %Area to perimeter ratio for an average cell (units = um)
 
 aByL = 0.4313; %Area to perimeter ratio for an average cell (units = um). Empirically derived - see AreaPerimeterMeasurements.xlsx for measurements
+F = 0.825; %The true coverage fraction of the monolayer, taking into account small gaps between cells. Empirically derived - see 
 
 boundImg = bwmorph(and(imdilate(imgS,strel('disk',2)),imdilate(imgK,strel('disk',2))),'skel',Inf);
 
 L = sum(boundImg(:)) * pxSize;
 As = sum(imgS(:)) * (pxSize^2);
 
-pK = aByL * (L/As);
+pK = (aByL/F) * (L/As);
